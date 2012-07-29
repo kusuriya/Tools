316227f6574e8da21c8ca982391497945dee310f
Jason Barbier <kusuriya@phobos.lan.serversave.us>
2012-07-29 10:08:38 -0700

    Adding youtube-dl script

---
+++ youtube.py
@@ -0,0 +1,18 @@
+#!env python
+import sys;
+import os;
+
+#Config
+videoplayer = "/usr/local/bin/mplayer";
+vidplayerargs = '-zoom -cache 1024 -vo x11';
+ytdl = "/usr/local/bin/youtube-dl";
+ytdlargs = ' -g ';
+
+#Commandline Var
+ytvid = sys.argv[1];
+
+#build command
+vid = videoplayer + " " + vidplayerargs + " " + "$(" + ytdl + " " + ytdlargs + " " + "'" + ytvid + "')";
+
+#execute
+os.system(vid);
